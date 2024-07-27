4cd6fd8764913feecb4b96d6f8e97e89d42a83bf
juan1 <juan1@Juan>
2024-07-27 12:52:26 -0400

    Primera fase del prototipo

---
+++ src/main/java/com/mycompany/sistema_prestamos/Sistema_Prestamos.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sistema_prestamos;
+
+/**
+ *
+ * @author juan1
+ */
+public class Sistema_Prestamos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
